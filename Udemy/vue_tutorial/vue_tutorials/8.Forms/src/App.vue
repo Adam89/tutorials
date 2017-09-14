1664984427671e37bf3c46fe0ae4f39a2b95a8fd
Adam Hopkins <adamhopkins87@gmail.com>
2017-09-14 22:56:38 +0100

    forms vue js

--- Udemy/vue_tutorial/vue_tutorials/8.Forms/src/App.vue
+++ Udemy/vue_tutorial/vue_tutorials/8.Forms/src/App.vue
@@ -10,33 +10,34 @@
                         <input
                                 type="text"
                                 id="email"
-                                class="form-control">
+                                class="form-control" v-model="userData.email">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input
                                 type="password"
                                 id="password"
-                                class="form-control">
+                                class="form-control" v-model.lazy="userData.password">
+                                {{ userData.password }}
                     </div>
                     <div class="form-group">
                         <label for="age">Age</label>
                         <input
                                 type="number"
                                 id="age"
-                                class="form-control">
+                                class="form-control" v-model="userData.age">
                     </div>
 
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="message">Message</label><br>
+                    <label for="message"></label><br>
                     <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
                     <textarea
                             id="message"
                             rows="5"
-                            class="form-control"></textarea>
+                            class="form-control" v-model="message"></textarea>
                 </div>
             </div>
             <div class="row">
@@ -46,13 +47,13 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail" v-model="sentMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail" v-model="sentMail"> Send Infomail
                         </label>
                     </div>
 
@@ -64,13 +65,13 @@
                         <input
                                 type="radio"
                                 id="male"
-                                value="Male"> Male
+                                value="Male" v-model="gender"> Male
                     </label>
                     <label for="female">
                         <input
                                 type="radio"
                                 id="female"
-                                value="Female"> Female
+                                value="Female"  v-model="gender"> Female
                     </label>
                 </div>
             </div>
@@ -101,15 +102,15 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Mail:</p>
-                        <p>Password:</p>
-                        <p>Age:</p>
-                        <p>Message: </p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Age: {{ userData.age }}</p>
+                        <p style="white-space: pre">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="item in sentMail">{{item}}</li>
                         </ul>
-                        <p>Gender:</p>
+                        <p>Gender: {{gender}}</p>
                         <p>Priority:</p>
                         <p>Switched:</p>
                     </div>
@@ -121,6 +122,20 @@
 
 <script>
     export default {
+        data() { //es6 style
+            return {
+                userData: { // object two way binding to a property within an object
+                    email: '', // two way binding
+                    password: '', // two way binding
+                    age: 27 // two way binding
+                },
+                message: 'A new text',
+                sentMail: [ ], // bind two inputs to same model which vue js detects auto merges the value into an array
+                gender: 'male'
+
+            }
+
+        }
     }
 </script>
 
--- Udemy/vue_tutorial/vue_tutorials/8.Forms/src/main.js
+++ Udemy/vue_tutorial/vue_tutorials/8.Forms/src/main.js
@@ -5,3 +5,31 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+
+/* input field will be hooked up to output at the end bind it with v-model and even if you bind the same v-model to it elsewhere vue js checks the source of the editing
+
+
+v-model connects html element with our property two way binding always add to the input element
+
+v-model reacts to input events so on every keystroke. You can modify the v-model to only update when user clicks out of input etc
+
+v-model.lazy reacts to a change event only fires when you leave the input
+
+other modifiers are
+
+v-model.trim to trim excess whitespace beginning an end
+
+v-model.number to force the input is converted to number straight away as the number is a string to start with in an input
+
+text area you can't use string interpolation to set default message you need to use v-model
+
+style="white-space: pre" allows you to keep line breaks in text area output from v-model
+
+bind radio buttons to a specific property and add both in one array
+
+bind two inputs to same model which vue js detects auto merges the value into an array
+
+vue js knows both radio buttons belong to same group gender vue js recognizes that they belong to same group so only one gets selected. vue auto stores the value in our gender property use the same model and vue js will take care of grouping and assigning value correctly.
+
+*/
