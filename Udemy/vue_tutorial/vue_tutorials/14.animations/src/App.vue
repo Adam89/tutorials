724a7f42c1af01bbfef2c9e1d81dbb4f8d46081c
Adam Hopkins <adamhopkins87@gmail.com>
2017-09-27 23:27:12 +0100

    filter and animations

---
+++ Udemy/vue_tutorial/vue_tutorials/14.animations/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <hr>
+                <button v-on:click="show = !show" class="btn btn-primary">Show Alert</button>
+                <br><br>
+                <transition name="fade">
+                <div v-if="show" class="alert alert-info">This is some info</div>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                show: false
+            }
+        }
+    }
+</script>
+
+<style>
+.fade-enter {
+    opacity: 0;
+}
+.fade-enter-active {
+    transition: opacity 2s;
+}
+.fade-leave {
+
+}
+.fade-leave-active {
+    transition: opacity 2s;
+    opacity: 0;
+}
+</style>
---
+++ Udemy/vue_tutorial/vue_tutorials/14.animations/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+
+
+/*
+
+vue offers us a transition wrapper <transition> </transition> anything in-between the tags can be animate however you can only animate one element
+
+vue js attaches css classes to the transition element in and out animation you have to have the element in your template with v-if or show
+
+
+<transition name="fade"> vue js auto attach fade enter  enter active leave leave active
+
+by adding fade name vue js can sinff out classes where you add transition
+
+.fade-enter only attached for 1 frame at start
+
+*/
