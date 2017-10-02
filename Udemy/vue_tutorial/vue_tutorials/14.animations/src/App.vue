8687146cd528d525ea8beb001e6f0688dac5303e
Adam Hopkins <adamh@khws.co.uk>
2017-10-02 14:54:53 +0100

    further animation changes

--- Udemy/vue_tutorial/vue_tutorials/14.animations/src/App.vue
+++ Udemy/vue_tutorial/vue_tutorials/14.animations/src/App.vue
@@ -4,16 +4,52 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
                 <hr>
+                <select name="" id="" v-model="alertAnimation" class="form-control">
+
+                    <option value="fade">fade</option>
+                    <option value="slide">slide</option>
+                </select>
+                <br>
                 <button v-on:click="show = !show" class="btn btn-primary">Show Alert</button>
                 <br><br>
-                <transition name="fade">
+                <transition v-bind:name="alertAnimation">
                 <div v-if="show" class="alert alert-info">This is some info</div>
                 </transition>
                 <hr>
                 <button v-on:click="showSlide = !showSlide" class="btn btn-primary">Show Alert Slide</button>
                 <br><br>
-                <transition name="slide" type="animation">
+                <transition v-bind:name="alertAnimation" type="animation">
                 <div v-if="showSlide" class="alert alert-info">This is some info sliding</div>
+                </transition>
+                <hr>
+                <button v-on:click="showLoad = !showLoad" class="btn btn-primary">Show Alert</button>
+                <transition
+                enter-active-class="animated bounce"
+                leave-active-class="animated shake" appear>
+                <div v-if="showLoad" class="alert alert-info">This is some info</div>
+                </transition>
+                <hr>
+                 <button v-on:click="showMultiple = !showMultiple" class="btn btn-primary">Show Alert</button>
+                <transition v-bind:name="alertAnimation" mode="out-in">
+                <div v-if="showMultiple" class="alert alert-info" key="info">This is some info</div>
+                <div v-else class="alert alert-warning" key="warning">Warning</div>
+                </transition>
+                <hr>
+                <h1>Javascript transition</h1>
+                <button class="btn btn-primary" v-on:click="load = !load">Load / Remove Element</button>
+                <br><br>
+                <transition v-on:before-enter="beforeEnter"
+                v-on:enter="enter"
+                v-on:after-enter="afterEnter"
+                v-on:enter-cancelled="afterEnter"
+                v-on:before-leave="beforeLeave"
+                v-on:leave="leave"
+                v-on:after-leave="afterLeave"
+                v-on:leave-cancelled="leaveCancelled">
+                    <div class="jsAnimation" v-if="load">
+
+                    </div>
+
                 </transition>
             </div>
         </div>
@@ -25,7 +61,43 @@
         data() {
             return {
                 show: false,
-                showSlide: false
+                showSlide: false,
+                showLoad: true,
+                alertAnimation: 'fade',
+                showMultiple: true,
+                load: false
+            }
+        },
+        methods: {
+            beforeEnter(el) { //es6 we receive an arg its the element on which the animation is performed
+                console.log('beforeEnter');
+
+            },
+            enter(el, done) {
+
+                console.log('enter');
+                done();
+            },
+            afterEnter(el) {
+                console.log('afterEnter');
+            },
+            enterCancelled(el) {
+                console.log('enterCancelled');
+            },
+            beforeLeave(el) { //es6 we receive an arg its the element on which the animation is performed
+                console.log('beforeleave');
+
+            },
+            leave(el, done) {
+
+                console.log('leave');
+                done();
+            },
+            afterLeave(el) {
+                console.log('afterleave');
+            },
+            leaveCancelled(el) {
+                console.log('leaveCancelled');
             }
         }
     }
@@ -50,17 +122,21 @@
 }
 .slide-enter-active {
     animation: slide-in 1s ease-out forwards;
-    transition: opacity .5s;
+    transition: opacity .1s;
 }
 .slide-leave {
 
 }
 .slide-leave-active {
     animation: slide-out 1s ease-out forwards;
-    transition: opacity .3s;
+    transition: opacity .1s;
     opacity: 0;
+}
 
-
+.jsAnimation {
+    width: 100px;
+    height: 100px;
+    background-color: red;
 }
 @keyframes slide-in {
     from {
--- Udemy/vue_tutorial/vue_tutorials/14.animations/src/main.js
+++ Udemy/vue_tutorial/vue_tutorials/14.animations/src/main.js
@@ -24,4 +24,36 @@ by adding fade name vue js can sinff out classes where you add transition
 
 type="animation" or transition set this to dictate who has the power on what runs
 
+v-show also works with animations aswell as v-if
+
+set appear attribute on the transition element to smoothly animate on load
+
+you can overwrite default classes associated with name you need to remove name and write your own classes enter-class or enter-active-class appear does not work with this custom classes yet
+
+you can bind the animation using v-bind:name="animation name from a select"
+
+
+you need to add a key if you have multiple divs and you want to animate and alternate between the two key="info" to stop this you can add two different modes v-show does not work on this only v-if else
+
+mode="out-in" -- let the old element animate out first before you animate the new one
+
+mode="in-out" does the opposite new in first before old animates out
+
+vue js allows us to listen to JS Transition event hooks to animate using JS
+
+enter when animation starts
+
+after-enter hook executed once animation is finished
+
+after enter canceled this is executed when a condition changes relating to the animation and before the animation has finished
+
+before leave - before the animation starts a leave process
+
+after leave - once animation leave
+
+after leave cancelled - something that changes the condition of the animation leave process
+
+call animation in the transition tag v-on:before-enter="SPECIFY FUNCTION TO RUN HERE"
+
+you can add css clases to all these hooks
 */
