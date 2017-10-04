a7b0eefa16697deb89fad2d8b892928a258e2f10
Adam Hopkins <adamh@khws.co.uk>
2017-10-04 14:45:59 +0100

    http request

--- Udemy/vue_tutorial/vue_tutorials/16.HTTP/src/App.vue
+++ Udemy/vue_tutorial/vue_tutorials/16.HTTP/src/App.vue
@@ -3,6 +3,20 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                	<label for="">Username</label>
+                	<input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                	<label for="">Mail</label>
+                	<input type="text" class="form-control" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" v-on:click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" v-on:click="fetchData">Get Data</button>
+                <ul class="list-group">
+                	<li class="list-group-item" v-for="u in users">{{u.username}} - {{u.email}}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -10,6 +24,42 @@
 
 <script>
     export default {
+    	data() {
+    		return {
+    			user: {
+    				username: '',
+    				email: ''
+    			},
+    			users:[]
+    		}
+    	},
+    	methods: {
+    		submit() { // send data to firebase
+    			// console.log(this.users);
+    			this.$http.post('https://vuejs-http-69e5f.firebaseio.com/data.json', this.users) // creates a data.json node
+    			.then( response => {// then is a method available on the promises approach function to be executed once post requests is sent and response received
+    				console.log(response);
+    			}, error => {
+    				console.log(error);
+    			});
+    		},
+    		fetchData() { // fetch data from firebase and store in an array
+    			this.$http.get('https://vuejs-http-69e5f.firebaseio.com/data.json')
+    			.then(response => { //vue resource gives us some helper methods on responses
+    				return response.json(); // all async tasks finished so you have acess to data below
+
+    		})
+    		.then(data => {
+    			const resultArray = [];
+    			for(let key in data) { //loop through cryptic keys strings
+    				resultArray.push(data[key]) // push data acess key and get value
+    				console.log(resultArray);
+    			}
+    			this.users = resultArray; // set users arrau to result array
+
+    		});
+    		}
+    	}
     }
 </script>
 
--- Udemy/vue_tutorial/vue_tutorials/16.HTTP/src/main.js
+++ Udemy/vue_tutorial/vue_tutorials/16.HTTP/src/main.js
@@ -7,14 +7,36 @@ Vue.use(VueResource) // tells vue js to add a plugin to core vue functionality
 new Vue({
   el: '#app',
   render: h => h(App)
-})
-
-
-
-/*
- VUE JS allows any ajax library to use jquery etc but VUE JS has it's own HTTP request library called VUE RESOURCE
+});
 
 
 
 
-/*
+/* VUE JS allows any ajax library to use jquery etc but VUE JS has it's own HTTP request library called VUE RESOURCE
+
+Firesbase allows us to have a BE for our app
+
+https://vuejs-http-69e5f.firebaseio.com/
+
+every vue instance has acess to vue Resource this.$http object
+you have many different methods after http get post
+
+post takes two args api end point and data get request only needs 1 arg api end point
+
+vue resource uses the promises approach so on a post the response promises us some data in the future
+
+then is a method avaliable on the promises approach returns a response as a paremeter and a 2nd arg of error
+
+vue js only sends the real object to DB it needs a unique cryptic id so we can append data to DB
+
+GET METHODS HELPERS
+
+response.json() first it gives us a promise as it doesn't have data straight away because it async so have to chain then methods to extract
+
+
+extracts body of response and converts it to json so front end can use it
+
+vue js clever enough to update dom on get request when it changes
+*/
+
+
