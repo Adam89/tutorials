c90c1f954a98db73ea0d12b84b3d0a15f3cb6a9d
Adam Hopkins <adamh@khws.co.uk>
2017-11-27 15:16:29 +0000

    more actions

--- Udemy/ReduxSimpleStarter-master/src/actions/index.js
+++ Udemy/ReduxSimpleStarter-master/src/actions/index.js
@@ -1,5 +1,12 @@
 export function selectBook(book) { // simple function which takes object book and title
     console.log('A book has been selected', book.title)
+        // selectBook is an Action creater needs to return action an object with type property
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book
+    }
 }
 
-// this is bound to book list container
+// actions has two values a type and payload confirms the condition of the action. Every action must have a type that described the action
+
+//type is usually uppercase
