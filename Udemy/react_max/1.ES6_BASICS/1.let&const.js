ac4e8b351c5983c6e75c069bb62ca53e9e149117
Adam Hopkins <adamhopkins87@gmail.com>
2018-02-28 21:45:41 +0000

    es6

---
+++ Udemy/react_max/1.ES6_BASICS/1.let&const.js
@@ -0,0 +1,37 @@
+/* 
+
+-------------------------------------------------------------------------
+LET AND CONST
+-------------------------------------------------------------------------
+
+let and const are new variables. 
+
+let: is the new var it can be mutated
+
+const: cannot be mutated. Use it for constant variables
+
+
+-------------------------------------------------------------------------
+Arrow Functions
+-------------------------------------------------------------------------
+
+no arguments needs an empty pair of parenthese
+1 argument does not need paretheses const function = name => { console.lot(name)}
+more than 1 arg needs parenthese
+
+if you just return a statement and have no other code in the body you can write a shorthand method you can omit parenthese for one parameter and also omit curly braces
+
+const multiplyNumbers = number => number * 2; multiplyNumbers(100);
+
+
+-------------------------------------------------------------------------
+EXPORT AMD IMPORT STATEMENTS
+-------------------------------------------------------------------------
+any exports with default set can be imported using any name because the default is associated to the name
+any exports not with defaul have to be imported using the name of the function or variable 
+you can also import all data using import * bundles from ' ' this returns an object allowing you to use bundled.whatever
+
+
+
+
+*/
