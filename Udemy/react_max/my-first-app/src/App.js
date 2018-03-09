00b027c82cc6e619432d32a0344281ffd420d6a8
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2018-03-09 16:54:23 +0000

    props

--- Udemy/react_max/my-first-app/src/App.js
+++ Udemy/react_max/my-first-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,9 @@ class App extends Component {
     return (
       <div className="App">
        <h1>Hi, This is a React App built by Adam</h1>
+       <Person name="Adam" age="28"/>
+        <Person name="Kelly" age="28"/>
+        <Person name="Tom" age="40"/>
       </div>
     );
   }
