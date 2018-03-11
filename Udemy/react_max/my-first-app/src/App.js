b2363fadff111ecebe8d24002c825b77d51c92ae
Adam Hopkins <adamhopkins87@gmail.com>
2018-03-11 09:28:46 +0000

    state

--- Udemy/react_max/my-first-app/src/App.js
+++ Udemy/react_max/my-first-app/src/App.js
@@ -3,13 +3,31 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Adam', age: 28},
+      {name: 'Kelly', age: 28},
+      {name: 'Tom', age: 40}
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('was clicked');
+    this.setState({persons: [
+      {name: 'JAUUUA', age: 28},
+      {name: 'Kelly', age: 28},
+      {name: 'Tom', age: 40}
+    ]})
+  }
+
   render() {
     return (
       <div className="App">
        <h1>Hi, This is a React App built by Adam</h1>
-       <Person name="Adam" age="28"/>
-        <Person name="Kelly" age="28"/>
-        <Person name="Tom" age="40"/>
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>& My Hobbie is making Adam Laugh </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
@@ -17,3 +35,16 @@ class App extends Component {
 
 
 export default App;
+
+
+// class has properties like a variable of a class
+
+// state is managed inside a class based component only. It is a javascript object array or object literal.
+
+// if state changes it re renders / update the dom
+
+// assigned switchNameHandler property to a function passed as reference 
+
+// never mutate state use this.setState it takes an object as an argument looks at new state and old state and only re renders difference this includes props changing too
+
+// functional components don't manipulate the state this is good. try to use them as much as possible
--- Udemy/react_max/my-first-app/src/Person/Person.js
+++ Udemy/react_max/my-first-app/src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const person  = (props) => { // es6 functional component
-    return <p>I'm {props.name} and I am {props.age} years old</p>
+    return (
+    <div>
+        <p>I'm {props.name} and I am {props.age} years old</p>
+        <p>{props.children}</p>
+    </div>
+    )
 }
 
 export default person // export the default
+
+// React gives us to to props.children which it refers to any elements between opening and closing tag.
