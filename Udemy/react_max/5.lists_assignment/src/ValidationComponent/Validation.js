ca0e779afe683ce55af13c5a9bbfbba4546921f5
Adam Hopkins <adamhopkins87@gmail.com>
2018-03-21 21:03:03 +0000

    new lists

--- Udemy/react_max/5.lists_assignment/src/ValidationComponent/Validation.js
+++ Udemy/react_max/5.lists_assignment/src/ValidationComponent/Validation.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const validation = (props) => {
-
     let validationMessage = 'Text long enough';
-
     if (props.inputLength <= 5) {
         validationMessage = 'Text tooo short';
     }
