d51ca90837d460d7a011d0f530a0c1d72ef6fcb9
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2018-06-01 17:27:16 +0100

    finished nav

--- Udemy/react_max/8.burger_project/src/components/Layout/Layout.js
+++ Udemy/react_max/8.burger_project/src/components/Layout/Layout.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
 const layout = (props) => ( //use layout componet as wrapper around core content component we want rendered on screen
 	<Aux>
-		<div>
-			Toolbar,SideDrawer, BackDrop
-		</div>  {/* holds three components Toolbar,SideDrawer, BackDrop */}
+		<Toolbar/>  {/* holds three components Toolbar,SideDrawer, BackDrop */}
 		<main className={classes.Content}>
 			{props.children}
 		</main> {/* output component we wrap with this layout */}
