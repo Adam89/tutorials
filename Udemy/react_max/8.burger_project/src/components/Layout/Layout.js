5c95ac84819c03ce11ffbbe777d859291fde8480
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2018-06-04 14:33:47 +0100

    started sidedrawer

--- Udemy/react_max/8.burger_project/src/components/Layout/Layout.js
+++ Udemy/react_max/8.burger_project/src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 const layout = (props) => ( //use layout componet as wrapper around core content component we want rendered on screen
 	<Aux>
 		<Toolbar/>  {/* holds three components Toolbar,SideDrawer, BackDrop */}
+		<SideDrawer />
 		<main className={classes.Content}>
 			{props.children}
 		</main> {/* output component we wrap with this layout */}
