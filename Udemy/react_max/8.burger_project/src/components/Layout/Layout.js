ad2af5d57c20d5d00ff09d0c3bffb45ca7209c2a
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2018-06-05 14:23:29 +0100

    sidedraw toggle

--- Udemy/react_max/8.burger_project/src/components/Layout/Layout.js
+++ Udemy/react_max/8.burger_project/src/components/Layout/Layout.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = (props) => ( //use layout componet as wrapper around core content component we want rendered on screen
-	<Aux>
-		<Toolbar/>  {/* holds three components Toolbar,SideDrawer, BackDrop */}
-		<SideDrawer />
-		<main className={classes.Content}>
-			{props.children}
-		</main> {/* output component we wrap with this layout */}
-	</Aux>
-);
+class Layout extends Component { //use layout componet as wrapper around core content component we want rendered on screen
+	state = {
+		showSideDrawer: true
+	}
+
+	sideDrawClosedHandler = () => {
+		this.setState({ showSideDrawer: false})
+	}
+
+	render () {
+		return (
+			<Aux>
+				<Toolbar />  {/* holds three components Toolbar,SideDrawer, BackDrop */}
+				<SideDrawer 
+				open={this.state.showSideDrawer} 
+				closed={this.sideDrawClosedHandler} />
+				<main className={classes.Content}>
+					{this.props.children}
+				</main> {/* output component we wrap with this layout */}
+			</Aux>
+		);
+	}
+}
 
 
-export default layout;
+export default Layout;
 
