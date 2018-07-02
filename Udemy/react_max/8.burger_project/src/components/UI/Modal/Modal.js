3a42a759bbed278dd22d3a3f80b6b86c1ae9241b
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2018-07-02 17:31:46 +0100

    added in spinner

--- Udemy/react_max/8.burger_project/src/components/UI/Modal/Modal.js
+++ Udemy/react_max/8.burger_project/src/components/UI/Modal/Modal.js
@@ -6,7 +6,7 @@ import Backdrop from '../Backdrop/Backdrop';
 class Modal extends Component {
 
   shouldComponentUpdate (nextProps, nextState) {
-    return nextProps.show !== this.props.show // should eval to true
+    return nextProps.show !== this.props.show || nextProps.children !== this.props.children// should eval to true
   }
 
   componentWillUpdate() {
--- Udemy/react_max/8.burger_project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Udemy/react_max/8.burger_project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,12 +95,15 @@ class BurgerBuilder extends Component {
             },
             deliveryMethod: 'fast'
         }
- // .json for firebase
-        axios.post('/orders.json', order)
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
-    }
 
+        axios.post('/orders.json', order)  // .json for firebase
+            .then(response => {
+                this.setState({ loading: false, purchasing: false });
+            })
+            .catch(error => {
+                this.setState({ loading: false, purchasing: false });
+            });
+    }
     render () {
         const disiabledInfo = {
             ...this.state.ingredients
