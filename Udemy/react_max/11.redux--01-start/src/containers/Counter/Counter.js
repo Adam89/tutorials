51ff80480c8e232d3304a4234a6401aad0248e44
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2018-10-18 17:33:53 +0100

    payload reducer

--- Udemy/react_max/11.redux--01-start/src/containers/Counter/Counter.js
+++ Udemy/react_max/11.redux--01-start/src/containers/Counter/Counter.js
@@ -5,32 +5,14 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
 
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
-
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
             </div>
         );
     }
@@ -40,11 +22,14 @@ const mapStateToProps =  state => {
   return {
     ctr: state.counter
   }
-}  // state managed to redux is mapped to the props exspect redux state as input and returns JS object map prop names and slices of state stored in redux
+}  // state managed to redux is mapped to the props expect redux state as input and returns JS object map prop names and slices of state stored in redux
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIncrementCounter: () => dispatch({type: 'INCREMENT'}) // dispatch action call using this.props
+    onIncrementCounter: () => dispatch({type: 'INCREMENT'}), // dispatch action call using this.props
+    onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+    onAddCounter: () => dispatch({type: 'ADD', payload: { val : 10}}),
+    onSubtractCounter: () => dispatch({type: 'SUBTRACT', payload: { val : 15}})
   };
 } // which actions do i want to dispatch in this container stores a func which recieves the dispacth func as an arg return JS Obj which has prop names hold ref to func which gets executed
 
--- Udemy/react_max/11.redux--01-start/src/store/reducer.js
+++ Udemy/react_max/11.redux--01-start/src/store/reducer.js
@@ -3,12 +3,30 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+  const newState = {...state}
+  const newStatePlus = newState
   if(action.type === 'INCREMENT') {
-    const newState = {...state}
-    const newStatePlus = newState.counter + 1
     // counter: state.counter + 1
     return {
-      counter:newStatePlus
+      counter: newStatePlus.counter + 1
+    }
+  }
+  if(action.type === 'DECREMENT') {
+    // counter: state.counter + 1
+    return {
+      counter: newStatePlus.counter - 1
+    }
+  }
+  if(action.type === 'SUBTRACT') {
+    // counter: state.counter + 1
+    return {
+      counter: newStatePlus.counter - action.payload.val
+    }
+  }
+  if(action.type === 'ADD') {
+    // counter: state.counter + 1
+    return {
+      counter: newStatePlus.counter + action.payload.val
     }
   }
   return state;
