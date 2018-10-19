e99589d461a70dc9d0fe4b3df0b17ba0eaaeb95b
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2018-10-19 17:56:48 +0100

    react

--- Udemy/react_max/11.redux--01-start/src/store/reducer.js
+++ Udemy/react_max/11.redux--01-start/src/store/reducer.js
@@ -1,34 +1,27 @@
 const initialState = {
   counter: 0,
 };
-
 const reducer = (state = initialState, action) => {
   const newState = {...state}
   const newStatePlus = newState
-  if(action.type === 'INCREMENT') {
-    // counter: state.counter + 1
-    return {
-      counter: newStatePlus.counter + 1
+    switch(action.type) { 
+      case 'INCREMENT':
+        return {
+          counter: newStatePlus.counter + 1
+        }
+      case 'DECREMENT':
+        return {
+          counter: newStatePlus.counter - 1
+        }
+      case 'SUBTRACT':
+        return {
+          counter: newStatePlus.counter - action.payload.val
+        }
+      case 'ADD':
+        return {
+          counter: newStatePlus.counter + action.payload.val
+        }
     }
-  }
-  if(action.type === 'DECREMENT') {
-    // counter: state.counter + 1
-    return {
-      counter: newStatePlus.counter - 1
-    }
-  }
-  if(action.type === 'SUBTRACT') {
-    // counter: state.counter + 1
-    return {
-      counter: newStatePlus.counter - action.payload.val
-    }
-  }
-  if(action.type === 'ADD') {
-    // counter: state.counter + 1
-    return {
-      counter: newStatePlus.counter + action.payload.val
-    }
-  }
   return state;
 };
 
