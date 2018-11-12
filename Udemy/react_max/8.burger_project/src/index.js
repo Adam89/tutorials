61c4d1fb7235019e36aac3864de9c027a5497b07
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2018-11-12 15:00:41 +0000

    remove content

--- Udemy/react_max/8.burger_project/src/index.js
+++ Udemy/react_max/8.burger_project/src/index.js
@@ -14,7 +14,7 @@ const rootReducer = combineReducers({
 
 
 const store = createStore(
-  rootReducer,,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
