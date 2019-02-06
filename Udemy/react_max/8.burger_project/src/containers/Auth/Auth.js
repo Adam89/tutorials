cf8f6081b359dfb15a7832087df3e34aceab6f3d
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-02-06 18:05:29 +0000

    auth started

--- Udemy/react_max/8.burger_project/src/containers/Auth/Auth.js
+++ Udemy/react_max/8.burger_project/src/containers/Auth/Auth.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import classes from './Auth.css';
+import * as actions from '../../store/actions/index'
 
 class Auth extends Component {
   state = {
@@ -18,15 +23,94 @@ class Auth extends Component {
         errorMsg: 'Please enter a valid name',
         touched: false
       },
+      password: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'password',
+          placeholder: 'Your password'
+        },
+        value: '',
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        errorMsg: 'Please enter a valid name',
+        touched: false
+      },
     }
   }
-  render() {
-    return (
-      <div>
-        <form>
 
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid // if empty string
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid
+    }
+    if (rules.maxLength) {
+      isValid = value.length >= rules.maxLength && isValid
+    }
+
+    return isValid
+  }
+
+  inputChangeHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]:{
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(event.target.value, this.state.controls[controlName]),
+        touched: true
+      }
+    }
+    this.setState({controls: updatedControls});
+  }
+  
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+  }
+
+
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.controls) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.controls[key]
+      })
+    }
+
+    const form = formElementsArray.map(formElement => (
+      <Input
+        key={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        errorMsg={formElement.config.errorMsg}
+        touched={formElement.config.touched}
+        change={(event) => this.inputChangeHandler(event, formElement.id)} />
+    ));
+    return (
+      <div className={classes.Auth}>
+        <form onSubmit={this.submitHandler}>
+          {form}
+          <Button btnType="Success">SUBMIT</Button>
         </form>
       </div>
     );
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth())
+  }
+}
+
+export default connect(null,  mapDispatchToProps)(Auth);
