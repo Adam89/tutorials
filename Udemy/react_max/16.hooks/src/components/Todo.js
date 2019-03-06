3fae55745b639cef3e75e41fbadad294959e57a8
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-03-06 15:53:47 +0000

    useeffect

--- Udemy/react_max/16.hooks/src/components/Todo.js
+++ Udemy/react_max/16.hooks/src/components/Todo.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'; // use state is a func takes inital state returns array with two ele cur state second ele is function to manipulate state
+import React, { useState, useEffect } from 'react'; // use state is a func takes inital state returns array with two ele cur state second ele is function to manipulate state
+import axios from 'axios';
 
 const todo = props => {
 
@@ -8,7 +9,19 @@ const todo = props => {
   //   userInput: '',
   //   todoList: []
   // });
-
+  useEffect(() => { // func executed on load for the first time hooks into reacts internals and makes sure code executes after render cycle for high performance and ui is updated correctly
+    axios
+    .get('https://hooks-1542a.firebaseio.com/todos.json')
+    .then((result) => {
+      console.log(result, 'result');
+      const todoData = result.data;
+      const todos = [];
+      for (const key in todoData) {
+        todos.push({id:key, name:todoData[key].name})
+      }
+      setTodoList(todos);
+    });
+  }, [])
   const inputChangeHandler = (event) => {
       setTodoName(event.target.value)
     //   setTodoState({
@@ -23,6 +36,14 @@ const todo = props => {
     //   userInput: toDoState.userInput,
     //   todoList: toDoState.todoList.concat(toDoState.userInput)
     // })
+    axios
+    .post('https://hooks-1542a.firebaseio.com/todos.json', {name: todoName})
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
   };
 
   return (
@@ -42,9 +63,13 @@ const todo = props => {
           {toDoState.todoList.map(todo => (
           <li key={todo}>{todo}</li>
         ))}
-         */}
+        }
+        
         {toDoList.map(todo => (
           <li key={todo}>{todo}</li>
+        ))*/}
+        {toDoList.map(todo => (
+          <li key={todo.id}>{todo.name}</li>
         ))}
       </ul>
     </React.Fragment>
