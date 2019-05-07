0e255edce10d19fe0b4001b113c2b9748df46fd5
Adam Hopkins <adamhopkins87@gmail.com>
2019-05-07 18:55:07 +0100

    bdd started

--- Udemy/tdd/giftgiver/src/components/App.test.js
+++ Udemy/tdd/giftgiver/src/components/App.test.js
@@ -3,10 +3,33 @@ import {shallow} from 'enzyme';
 import App from './App';
 
 
-const app = shallow(<App/>);
 
-describe('@app', () => {
+describe('@App', () => {
+  const app = shallow(<App/>);
+
   it('renders correctly', () => {
     expect(app).toMatchSnapshot()
-  })
-})
+  });
+
+  it('initializes the state with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual( [] )
+  });
+
+  describe('when clicking the `add-gift` button', () => {
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click')
+    })
+
+    afterEach(() => {
+      app.setState({ gifts: [] })
+    });
+
+    it('adds a new gift to `state` when clicking `add gift button`', () => {
+      expect(app.state().gifts).toEqual([ {id:1} ])
+    });
+
+    it('add a new gift to the rendered list when clicking the `add gift` button', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
+  });
+});
